6858bedd3e35db43344c6cc7352590e54308443a
adarsh1997shah <adarshshah01@gmail.com>
2020-07-24 19:45:38 +0530

    fix login logout

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,6 +1,7 @@
-// Sign-up.
 const signupForm = document.getElementById( 'signup-form' );
 const preloader = document.querySelector( '.preloader-background' );
+const logout = document.getElementById( 'logout' );
+const loginForm = document.getElementById( 'login-form' )
 
 
 // Sign-up callback.
@@ -26,10 +27,115 @@ function signupSubmit( e ) {
 
 		// Reset the form value.
 		signupForm.reset();
+
 	} ).catch( err => {
+		// Closing preloader.
+		preloader.classList.remove( 'active' );
+
+		// Reset the form value.
+		signupForm.reset();
+
 		console.log( 'Opps you got an error!', err.message );
 	} );
 }
 
-// Listen for form submit.
+
+// Logout callback.
+function handleLogout( e ) {
+	e.preventDefault();
+
+	// Activate preloader.
+	preloader.classList.add( 'active' );
+
+	auth.signOut().then( () => {
+
+		// Closing preloader.
+		preloader.classList.remove( 'active' );
+
+	} ).catch( err => {
+		console.log( 'Error message' + err.message );
+	} );
+}
+
+
+// Login callback.
+function loginSubmit( e ) {
+	e.preventDefault();
+
+	// Get user info.
+	const loginEmail = this.loginemail.value;
+	const loginPassword = this.loginpassword.value;
+
+
+	// Activate preloader.
+	preloader.classList.add( 'active' );
+
+
+	// log in the user.
+	auth.signInWithEmailAndPassword( loginEmail, loginPassword ).then( credential => {
+
+		// Closing preloader.
+		preloader.classList.remove( 'active' );
+
+		// Closing the modal after login up.
+		const modal = document.getElementById( 'modal-login' );
+		M.Modal.getInstance( modal ).close();
+
+		// Reset the form value.
+		loginForm.reset();
+
+	} ).catch( err => {
+		// Closing preloader.
+		preloader.classList.remove( 'active' );
+
+		// Reset the form value.
+		loginForm.reset();
+
+		console.log( 'Opps you got an error!', err.message );
+	} );
+}
+
+
+// Listen for auth status.
+auth.onAuthStateChanged( user => {
+
+	if( user ) {
+		console.log( 'user has logged in', user );
+
+		// Activate preloader.
+		preloader.classList.add( 'active' );
+
+		renderLinks( user );
+
+		// Getting data from firestore.
+		// db.collection( 'guides' ).onSnapshot( ( snapshot ) => {
+
+		// 	// Closing preloader.
+		preloader.classList.remove( 'active' );
+
+		// 	renderGuide( snapshot );
+
+		// }, ( error ) => {
+		// 	console.log( 'Opps you have an error: ' + error.message );
+		// } );
+	} else {
+
+		// Closing preloader.
+		preloader.classList.remove( 'active' );
+
+		renderGuide();
+		renderLinks();
+		console.log( 'user logged out' );
+	}
+} );
+
+
+
+// Listen for sign-up form submit.
 signupForm.addEventListener( 'submit', signupSubmit );
+
+// Listen for logout button.
+logout.addEventListener( 'click', handleLogout );
+
+// Listen for log-in form submit.
+loginForm.addEventListener( 'submit', loginSubmit );
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+const guides = document.querySelector( '.guides' );
+const loggedInLinks = document.querySelectorAll( '.logged-in' );
+const loggedOutLinks = document.querySelectorAll( '.logged-out' );
+
+
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
 
@@ -6,5 +11,50 @@ document.addEventListener('DOMContentLoaded', function() {
   
 	var items = document.querySelectorAll('.collapsible');
 	M.Collapsible.init(items);
+
+	// Activate preloader.
+	preloader.classList.add( 'active' );
   
 });
+
+// Rendering the guides.
+function renderGuide( snapshot ) {
+
+	guides.innerHTML = '';
+
+	if( snapshot ) {
+
+		snapshot.docChanges().forEach( change => {
+
+			if( change.type === 'added' ) {
+				let guide = change.doc.data();
+
+				guides.innerHTML += 
+					`<li>
+						<div class="collapsible-header grey lighten-4">${guide.title}</div>
+						<div class="collapsible-body white">${guide.content}</div>
+					</li>`;
+			}
+		} );
+	} else {
+		guides.innerHTML = `<li><h4 class="center">Login to view Guides<h4></li>`;
+	}
+}
+
+
+
+// Setting up links render on login and logout.
+function renderLinks( user ) {
+
+	// If user is logged in.
+	if( user ) {
+
+		loggedInLinks.forEach( link => link.style.display = 'block' );
+		loggedOutLinks.forEach( link => link.style.display = 'none' );
+
+	} else {
+
+		loggedInLinks.forEach( link => link.style.display = 'none' );
+		loggedOutLinks.forEach( link => link.style.display = 'block' );
+	}
+}
